ca2e0435bb2c1d94b6c429ab17094e675ef52fed
LipatovAV <lipa-andrej@yandex.ru>
2018-12-08 20:34:42 +0300

    make it better

---
+++ src/main/java/com/example/instex/InstexApplication.java
@@ -0,0 +1,12 @@
+package com.example.instex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstexApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InstexApplication.class, args);
+    }
+}
