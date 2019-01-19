f4e1732373117d12c4e0fccdfa1ff482b57d0cad
LipatovAV <lipa-andrej@yandex.ru>
2019-01-19 14:14:56 +0300

    добавлен CrossOrigin

--- src/main/java/com/example/instex/controllers/FirstController.java
+++ src/main/java/com/example/instex/controllers/FirstController.java
@@ -3,10 +3,12 @@ package com.example.instex.controllers;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
 @Slf4j
 public class FirstController {
 
